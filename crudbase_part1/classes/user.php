06515f4a7922e14e000a7f0a756c7ff3bb7d2b15
paolo berlanda <paolo.berlanda@marconirovereto.it>
2023-04-03 14:05:53 +0200

    sono arrivato al min 15:20 del video. creazione db, classe database.php e classe user.php che è da finire

--- crudbase_part1/classes/user.php
+++ crudbase_part1/classes/user.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'database.php';
+require_once 'database.php'; // richiama la classe database.php
 
 class User {
     private $conn;
@@ -8,21 +8,21 @@ class User {
     // Constructor
     public function __construct(){
       $database = new Database();
-      $db = $database->dbConnection();
+      $db = $database->dbConnection(); // richiama il metodo dbConnection dalla classe database.php
       $this->conn = $db;
     }
 
 
     // Execute queries SQL
     public function runQuery($sql){
-      $stmt = $this->conn->prepare($sql);
+      $stmt = $this->conn->prepare($sql); //stmt significa statement prepare() è prepared statement, vedi qui https://www.w3schools.com/php/php_mysql_prepared_statements.asp
       return $stmt;
     }
 
-    // Insert
+    // Insert (nuovo utente)
     public function insert($name, $email){
       try{
-        $stmt = $this->conn->prepare("INSERT INTO crud_users (name, email) VALUES(:name, :email)");
+        $stmt = $this->conn->prepare("INSERT INTO crud_users (name, email) VALUES(:name, :email)"); // come vei qui si usa il prepared statement
         $stmt->bindparam(":name", $name);
         $stmt->bindparam(":email", $email);
         $stmt->execute();
@@ -34,6 +34,19 @@ class User {
 
 
     // Update
+
+    public function update($id, $name, $email){
+      try{
+        $stmt = $this->conn->prepare("UPDATE crud_users SET name = :name, name = :email WHERE id = :id");
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return $stmt;
+      } catch(PDOException $e){
+
+      }
+    }
 
 
     // Delete
