971a76ed42f198ba0dcff68386fbdc938e4f0bbd
paolo berlanda <paolo.berlanda@marconirovereto.it>
2023-04-04 17:38:52 +0200

    Aggiunto codice per visualizzazione e inserimento ma a errore

--- crudbase_part1/form.php
+++ crudbase_part1/form.php
@@ -4,10 +4,44 @@ ini_set('display_errors',1);
 ini_set('display_startup_erros',1);
 error_reporting(E_ALL);
 
-// GET
+require_once 'classes/user.php';
 
+$objUser = new User();
+
+// GET
+if (isset($_GET['edit_id'])) {
+    $id = $_GET['edit_id'];
+    $stmt = $objUser->runQuery("SELECT * FROM crud_users WHERE id=:id");
+    
+    $stmt->execute(array(":id" => $id));
+    $rowUser = $stmt->fetch(PDO::FETCH_ASSOC);
+} else {
+    $id = null;
+    $rowUser = null;
+}
 
 // POST
+if (isset($_POST['btn_save'])){
+    $name  = strip_tags($_POST['name']);
+    $email = strip_tags($_POST['email']);
+
+    try {
+        if($id != null){
+            if ($objUser->update($name, $email, $id)) {
+                $objUser->redirect('index.php?updated');
+            } 
+        } else {
+            if ($objUser->insert($name, $email)) {
+                $objUser->redirect('index.php?inserted');
+            } else {
+                $objUser->redirect('index.php?error');
+            }
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
 
 ?>
 <!doctype html>
@@ -29,19 +63,19 @@ error_reporting(E_ALL);
                     <form method="post">
                         <div class="form-group">
                             <label for="id">ID</label>
-                            <input class="form-control" type="text" name="id" id="id" id value="" readonly>
+                            <input class="form-control" type="text" name="id" id="id" value="<?php print($rowUser['id']); ?>" readonly>
                         </div>
                         <div class="form-group">
                             <label for="name">Name *</label>
-                            <input class="form-control" type="text" name="name" id="name" id value="" placeholder="nome e cognome" required maxlength="100">
+                            <input class="form-control" type="text" name="name" id="name" value="<?php print($rowUser['name']); ?>" placeholder="nome e cognome" required maxlength="100">
                         </div>
                         <div class="form-group">
                             <label for="email">Email *</label>
-                            <input class="form-control" type="text" name="email" id="email" id value="" placeholder="guidolape@email.com" maxlength="100">
+                            <input class="form-control" type="text" name="email" id="email" value="<?php print($rowUser['email']); ?>" placeholder="guidolape@email.com" maxlength="100">
                         </div>
                     </form>
 
-                    <input class="btn btn-primary mb-2" type="button" name="btn_save" value="Salva">
+                    <input class="btn btn-primary mb-2" type="submit" name="btn_save" value="Salva">
                 </main>
             </div>
         </div>
--- crudbase_part1/index.php
+++ crudbase_part1/index.php
@@ -4,6 +4,10 @@ ini_set('display_errors',1);
 ini_set('display_startup_erros',1);
 error_reporting(E_ALL);
 
+require_once 'classes/user.php';
+
+$objUser = new User();
+
 // GET
 
 ?>
@@ -23,6 +27,49 @@ error_reporting(E_ALL);
                 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
                     <h1 style="margin-top: 10px">DataTable</h1>
 
+                    <div class="table-responsive">
+                        <table class="table table-striped table-sm">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nome</th>
+                                    <th>Email</th>
+                                    <th></th> <!-- spazio per le azioni sull'utente -->
+                                </tr>
+                            </thead>
+                           <?php
+                            $query="SELECT * FROM crud_users";
+                            $stmt=$objUser->runQuery($query);
+                            $stmt->execute();
+                            ?> 
+                            <tbody>
+                                <?php if ($stmt->rowCount() > 0) {
+
+                                    // se ci sono utenti nel db popola la tabella
+                                    while ($rowUser = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                                ?>
+
+                                <td><?php print($rowUser['id']); ?></td>
+
+                                <!-- aggiungo un link al form per la modifica dell'utente. Recupero l'id dell'utente per passarlo a form.php -->
+                                <td>
+                                    <a href="form.php?edit_id=<?php print($rowUser['id']); ?>"> 
+                                    <?php print($rowUser['name']); ?>
+                                    </a>
+                                </td>
+
+                                <td><?php print($rowUser['email']); ?></td>
+
+                                <td>
+                                    <a href="index.php?delete_id=<?php print($rowUser['id']); ?>"> 
+                                    <span data-feather="trash"></span>
+                                    </a>
+                                </td>
+
+                            </tbody>
+                            <?php } }?>
+                        </table>
+                    </div> 
 
                 </main>
             </div>
