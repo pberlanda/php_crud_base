f31b3dd9f157fcbf5959575ff6dc80ab441be9e4
paolo berlanda <paolo.berlanda@marconirovereto.it>
2023-04-04 13:51:03 +0200

    creazione form crea/modifica utenti

--- crudbase_part1/form.php
+++ crudbase_part1/form.php
@@ -24,7 +24,24 @@ error_reporting(E_ALL);
                 <!-- Sidebar menu -->
                 <?php require_once 'includes/sidebar.php'; ?>
                 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-                    
+                    <h1 margin-top: 10px>Aggiungi / Modifica utenti</h1>
+                    <p>valori richiesti *</p>
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="id">ID</label>
+                            <input class="form-control" type="text" name="id" id="id" id value="" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Name *</label>
+                            <input class="form-control" type="text" name="name" id="name" id value="" placeholder="nome e cognome" required maxlength="100">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email *</label>
+                            <input class="form-control" type="text" name="email" id="email" id value="" placeholder="guidolape@email.com" maxlength="100">
+                        </div>
+                    </form>
+
+                    <input class="btn btn-primary mb-2" type="button" name="btn_save" value="Salva">
                 </main>
             </div>
         </div>
