949f7f0b8313c153d300ac666df026fadeb2fa61
paolo berlanda <paolo.berlanda@marconirovereto.it>
2023-04-13 13:11:04 +0200

    test modifica sidebar e header

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,14 +1,14 @@
 <nav class="col-md-2 d-none d-md-block bg-light sidebar">
     <div class="sidebar-sticky">
         <ul class="nav flex-column">
+        <li class="nav-item">
+                <a class="nav-link" href="index.php">utenti</a>
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="form.php">
                     <span data-feather="plus-circle"></span>
                     Aggiungi utente
                 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="form.php">test link 1</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="form.php">test link 2</a>
